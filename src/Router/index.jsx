c0623135365a14e530688c4755ee926585bfb208
Ehsan-Rohanii <ehsanrohani.82@gmail.com>
2026-08-25 15:44:46 +0330

    fix Router

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, Route } from "react-router";
 import Layout from "../Layout";
 import ProductDetails from "../Components/Products/ProductDetails";
-import Auth from "../Pages/Auth/FirstStep";
 import AllProducts from "../Pages/AllProducts";
+import Register from "../Pages/Auth/Register";
 
 const router = createBrowserRouter ([
     {
         path:'/',
-        element:<Auth/>
+        element:<Register/>
     },
     {
         path:'/home' ,
